6e46670470b00daa52a7a83fee87769256598898
Nicholas Merolle <nmerolle11@hotmail.com>
2021-01-07 23:48:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,11 +45,13 @@ class Student
   end
 
   def self.new_from_db(row)
-  self.new(id, name, grade)  id = row[0]
-    name = row[1]
-    grade = row[2]
-    
-  end 
+   attributes_hash = {
+      :id => row[0],
+      :name => row[1],
+      :breed => row[2]
+    }
+    self.new(attributes_hash)
+   end 
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
